fc8f3c94f98ac39b24488491a5607e2e3f4339ff
Clay Burnette <clayburnette13@gmail.com>
2022-05-19 09:41:33 -0400

    Fixed Azure Not Displaying My Bootstrap

--- PetRental.WebMVC1/App_Start/BundleConfig.cs
+++ PetRental.WebMVC1/App_Start/BundleConfig.cs
@@ -23,8 +23,8 @@ namespace PetRental.WebMVC1
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css"));
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
